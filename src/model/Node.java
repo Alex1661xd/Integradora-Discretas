bc28dd98a784a2c3fb930c5aa6c8fabf75226a6d
INTEGRADORAII <hectordelga16@gmail.com>
2023-09-25 15:59:48 -0500

    Metodo addTask, y HashTable

--- src/model/Node.java
+++ src/model/Node.java
@@ -1,36 +1,46 @@
 package model;
 
-public class Node<T> {
+public class Node<K, V> {
 
-    private T task;
-    private Node<T> next;
-    private Node<T> previous;
+    private K key;
+    private V value;
+    private Node<K, V> next;
+    private Node<K, V> previous;
 
-    public Node(T task) {
-        this.task = task;
+    public Node(K key, V value) {
+        this.key = key;
+        this.value = value;
     }
 
-    public T getTask() {
-        return task;
+    public K getKey() {
+        return key;
     }
 
-    public void setTask(T task) {
-        this.task = task;
+    public void setKey(K key) {
+        this.key = key;
     }
 
-    public Node<T> getNext() {
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public Node<K, V> getNext() {
         return next;
     }
 
-    public void setNext(Node<T> next) {
+    public void setNext(Node<K, V> next) {
         this.next = next;
     }
 
-    public Node<T> getPrevious() {
+    public Node<K, V> getPrevious() {
         return previous;
     }
 
-    public void setPrevious(Node<T> previous) {
+    public void setPrevious(Node<K, V> previous) {
         this.previous = previous;
     }
 }
--- src/model/RSystem.java
+++ src/model/RSystem.java
@@ -1,8 +1,30 @@
 package model;
-import java.util.ArrayList;
+
 
 public class RSystem {
- 
+
+    private HashTable<String, Task> hashTable;
+
+    public RSystem(){
+        this.hashTable= new HashTable<>(25);
+    }
     
-    
+    public void addTask(String Key, String title, String description, String fechaLimit, int priority, String identifier){
+        
+        TypePriority tipoPrioridad=null;
+        if(priority==1){
+            tipoPrioridad=TypePriority.PRIORITY;
+        }else{
+            tipoPrioridad=TypePriority.NO_PRIORITY;
+        }
+        //Crea una nueva tarea o recordatorio con los datos ingresados por el usuario
+        Task nuevaTarea=new Task(title, description, fechaLimit, tipoPrioridad, identifier);
+        //Inserta en la tabla hash  el nuevo nodo
+        hashTable.insert(identifier, nuevaTarea);
+
+    }
+
+    public void editTask(){
+        
+    }
 }
