393afc2d1958bb18649f8d35c1c45960f9a1ba5e
MarianaDeLaCruz06 <mariancruzpo@gmail.com>
2023-09-25 15:35:20 -0500

    Entrega

--- src/model/RSystem.java
+++ src/model/RSystem.java
@@ -2,12 +2,6 @@ package model;
 import java.util.ArrayList;
 
 public class RSystem {
-<<<<<<< HEAD
-    ArrayList<User> usuarios= new ArrayList<>();
 
-=======
- 
-    
->>>>>>> 2f6e88e86e02ad3378f22af6f48dabf3a7cc189e
-    
+
 }
