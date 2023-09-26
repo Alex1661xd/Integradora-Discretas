b7249e1b5ff8a5c4bf4fd70a8ab450c942c6c324
INTEGRADORAII <hectordelga16@gmail.com>
2023-09-26 16:07:57 -0500

    Cree el stack de historial

--- src/model/RSystem.java
+++ src/model/RSystem.java
@@ -5,11 +5,12 @@ public class RSystem {
 
     private HashTable<String, Task> hashTable;
     private Queue<Task> cola=new Queue<>();
+    private Stack<String> historialAcciones=new Stack<>();
 
     public RSystem(){
         this.hashTable= new HashTable<>(25);
 
-        //addTask("123A", "Español","Esta tarea es de poemas", "10/10/23",2);
+        addTask("123A", "Español","Esta tarea es de poemas", "10/10/23",2);
     }
     
     public boolean addTask(String key, String title, String description, String fechaLimit, int priority){
@@ -44,6 +45,29 @@ public class RSystem {
 
     public String printQueue(){
         return cola.printQueue();
+    }
+
+    public void addActionStack(String actionR, int optionEditTask, String identifier, int optionRegist){
+        String mensajeHistorial="";
+        //1 significa que desea registrar la creacion de una tarea
+        if(optionRegist==1){
+            mensajeHistorial=actionR+"\nEl identificador de la nueva tareas es: ["+identifier+"]";
+        }else if(optionRegist==2){
+            mensajeHistorial=actionR;
+                if(optionEditTask==1){
+                    mensajeHistorial+="\nEn la tarea se edito el Identificador";
+                }else if(optionEditTask==2){
+                    mensajeHistorial+="\nEn la tarea se edito el Titulo";
+                }else if(optionEditTask==3){
+                    mensajeHistorial+="\nEn la tarea se edito la descripcion";
+                }else{
+                    mensajeHistorial+="\nEn la tarea se edito la fecha";
+                }
+        }else if(optionEditTask==3){
+            mensajeHistorial+="\nEl usuario elimino una tarea con el identificador: "+identifier;
+        }
+
+        
     }
 
 }
