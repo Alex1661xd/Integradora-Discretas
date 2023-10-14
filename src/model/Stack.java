072337c95abf3b9d44509bfe8f81fc2c4b17369c
Valentinag9 <1107839938@u.icesi.edu.co>
2023-10-14 12:13:17 -0500

    Entrega

--- src/model/Stack.java
+++ src/model/Stack.java
@@ -1,36 +1,44 @@
 package model;
 
-public class Stack<T extends Comparable<T>> {
+public class Stack<T> {
     private Node<T> top;
 
-    public Stack() {
-        top = null;
+    private static class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
     }
+    //Añade un elemento a la cima
 
     public void push(T item) {
         Node<T> newNode = new Node<>(item);
         if (top == null) {
             top = newNode;
         } else {
-            newNode.setNext(top);
+            newNode.next = top;
             top = newNode;
         }
     }
-
+    //Elimina el elemento de la cima y el valor del elemto
     public T pop() {
         if (isEmpty()) {
-            throw new IllegalStateException("La pila está vacía.");
+            throw new IllegalStateException("El usuario no ha realizado acciones");
         }
-        T data = top.getData();
-        top = top.getNext();
+        T data = top.data;
+        top = top.next;
         return data;
     }
+    //Muestra el elemento de la cima
 
     public T peek() {
         if (isEmpty()) {
             throw new IllegalStateException("La pila está vacía.");
         }
-        return top.getData();
+        return top.data;
     }
 
     public boolean isEmpty() {
@@ -42,28 +50,27 @@ public class Stack<T extends Comparable<T>> {
         Node<T> current = top;
         while (current != null) {
             count++;
-            current = current.getNext();
+            current = current.next;
         }
         return count;
     }
-
     public Node<T> getTop() {
         return top;
     }
-
     public void setTop(Node<T> top) {
         this.top = top;
     }
 
     public String printStack() {
         Node<T> current = top;
-        StringBuilder msg = new StringBuilder("Stack:\n");
+        String msg="    Stack: ";
         while (current != null) {
-
             msg+=((Task)current.data).getDescriptionUseRealized() + "\n";
             current = current.next;
- 
         }
-        return msg.toString();
+        return msg;// Imprime una línea en blanco al final para una mejor presentación.
     }
+    
+
+    
 }
