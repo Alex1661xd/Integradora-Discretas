8a1511d4b04ac2e2c5f1b2d8f1b6a82ed5177d61
Valentinag9 <1107839938@u.icesi.edu.co>
2023-10-15 01:02:07 -0500

    Entrega

--- src/model/HashTable.java
+++ src/model/HashTable.java
@@ -11,88 +11,116 @@ public class HashTable<K, V> implements IHashTable<K,V> {
         this.table = new NodeHT[size];
     }
 
+    /**
+    * Calculates the corresponding index for the given key using a hash function.
+    *
+    * @param key The key for which the index will be calculated.
+    * @return The calculated index for the key.
+    */
     public int FunctionHash(K key) {
         return key.hashCode()%size;
     }
 
-    
+
+    /**
+     * Inserts a new key-value pair into the hash table.
+     *
+     * @param key The key to use to index the value.
+     * @param value The value associated with the key.
+    */
     @Override
     public void insert(K key, V value) {
-        int index = FunctionHash(key); // Calcula el índice utilizando la función hash
+        int index = FunctionHash(key); // Calculate the index using the hash function
     
-        NodeHT<K, V> newNode = new NodeHT<>(key, value); // Crea un nuevo nodo con la clave y el valor
+        NodeHT<K, V> newNode = new NodeHT<>(key, value); // Create a new node with the key and value
     
         if (table[index] == null) {
-            // Si no hay ningún nodo en ese índice, simplemente establece el nuevo nodo en ese índice
+            // If there is no node at that index, set the new node at that index
             table[index] = newNode;
         } else {
-            // Si ya hay un nodo en ese índice, coloca el nuevo nodo en la cabeza de la lista enlazada
+            // If there is already a node at that index, place the new node at the head of the linked list
             newNode.setNext(table[index]);
             table[index].setPrevious(newNode);
             table[index] = newNode;
         }
     }
-    
 
+
+    /**
+    * Looks up the value associated with a key in the hash table.
+    *
+    * @param key The key to search for.
+    * @return The value associated with the key or null if the key is not found in the table.
+    */
     @Override
     public V search(K key) {
-        int index = FunctionHash(key); // Calcula el índice utilizando la función hash
+        int index = FunctionHash(key); // Calculate the index using the hash function
     
         if (table[index] == null) {
-            // No se encontró un nodo en ese índice, devuelve null o realiza el manejo adecuado
+            // No node was found at that index, return null or do the appropriate handling
             return null;
         } else {
-            // Recorre la lista enlazada en ese índice para buscar la clave y devolver el valor asociado
+            // Loops through the linked list at that index to find the key and return the associated value
             NodeHT<K, V> currentNode = table[index];
             while (currentNode != null) {
                 if (currentNode.getKey().equals(key)) {
-                    return currentNode.getValue(); // Se encontró la clave, devuelve el valor asociado
+                    return currentNode.getValue(); // Key found, returns associated value
                 }
                 currentNode = currentNode.getNext();
             }
-            // La clave no se encontró en la lista enlazada, devuelve null o realiza el manejo adecuado
+            // Key not found in linked list, return null or do appropriate handling
             return null;
         }
     }
-    
 
+    /**
+    * Removes a key-value pair from the hash table.
+    *
+    * @param key The key of the pair to delete.
+    * @return true if the key-value pair was deleted successfully, false if the key was not found in the table.
+    */
     @Override
     public boolean delete(K key) {
-        int index = FunctionHash(key); // Calcula el índice utilizando la función hash
+        int index = FunctionHash(key); // Calculate the index using the hash function
     
         if (table[index] == null) {
-            // No se encontró un nodo en ese índice, no hay nada que eliminar
+            // No node was found in that index, there is nothing to delete
             return false;
         } else {
             NodeHT<K, V> currentNode = table[index];
             NodeHT<K, V> previousNode = null;
     
-            // Busca el nodo con la clave para eliminarlo
+            // Find the node with the key to delete it
             while (currentNode != null) {
                 if (currentNode.getKey().equals(key)) {
                     NodeHT<K, V> nextNode = currentNode.getNext();
     
-                    // Actualiza las referencias para eliminar el nodo
+                    // Update references to remove the node
                     if (previousNode != null) {
                         previousNode.setNext(nextNode);
                     } else {
-                        table[index] = nextNode; // Si es el primer nodo, actualiza la referencia de la tabla
+                        table[index] = nextNode; // If it is the first node, update the table reference
                     }
     
                     if (nextNode != null) {
                         nextNode.setPrevious(previousNode);
                     }
     
-                    return true; // Nodo eliminado exitosamente
+                    return true; // Node removed successfully
                 }
                 previousNode = currentNode;
                 currentNode = currentNode.getNext();
             }
         }
-        return false; // No se encontró el nodo con la clave
+        return false; // The node with the key was not found
     }
     
 
+    /**
+    * Gets a string representation of the hash table and its elements.
+    *
+    * @return A string representing the hash table information and its elements.
+    */
     @Override
     public String print() {
         StringBuilder stringBuilder = new StringBuilder();
@@ -116,9 +144,5 @@ public class HashTable<K, V> implements IHashTable<K,V> {
         
         return stringBuilder.toString();
     }
-
-
-    
-
 }
 
